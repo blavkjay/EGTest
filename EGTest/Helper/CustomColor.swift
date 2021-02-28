ca22018668203d0759e333daa5230fe946534cd4
Olajuwon Balogun <olajuwon@softcom.ng>
2021-02-28 22:01:28 +0100

    Restructure folder, add color sets

---
+++ EGTest/Helper/CustomColor.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColor.swift
+//  EGTest
+//
+//  Created by OLAJUWON BALOGUN on 28/02/2021.
+//
+
+import Foundation
+import UIKit
+
+class CustomColor {
+    static let green = UIColor(named: "green")
+    static let blue = UIColor(named: "blue")
+    static let textfieldGray = UIColor(named: "textfieldGray")
+    static let graphBlue = UIColor(named: "graphBlue")
+    static let textBlack = UIColor(named: "textBlack")
+}
